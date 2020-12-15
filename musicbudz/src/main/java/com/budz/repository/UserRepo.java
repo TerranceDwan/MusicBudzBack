0738b56cb96bf93e22f84a62890448f58e0bdf7b
TerranceDwan <terrance4869@gmail.com>
2020-12-15 15:39:00 -0500

    created user and relationship repos

--- musicbudz/src/main/java/com/budz/repository/UserRepo.java
+++ musicbudz/src/main/java/com/budz/repository/UserRepo.java
@@ -1,5 +1,17 @@
 package com.budz.repository;
 
+import java.util.ArrayList;
+
+import com.budz.models.User;
+
 public interface UserRepo {
 
+	void createUser(String email, String password, String userName, String firstName, String lastName);
+	ArrayList<User> getAllUsers();
+	User loginWithEmail(String email, String password);
+	User loginWithUsername(String username, String password);
+	User getUserData(int userId);
+	ArrayList<User> getFriends(int userId);
+	void updateUserData(User user);
+	void deleteAccount(int userId);
 }
