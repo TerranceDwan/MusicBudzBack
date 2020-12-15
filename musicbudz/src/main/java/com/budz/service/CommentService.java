4a449c1745d87b557920a3e6eddd04957553625e
J.R. Hyde <jrhyde17@gmail.com>
2020-12-15 16:34:46 -0500

    Outlined CommentRepo & CommentService

---
+++ musicbudz/src/main/java/com/budz/service/CommentService.java
@@ -0,0 +1,21 @@
+package com.budz.service;
+
+import java.util.ArrayList;
+
+import com.budz.models.Comment;
+import com.budz.models.Review;
+
+public class CommentService {
+	public void addComment(Comment comment) {
+		
+	}
+	public void deleteComment(Comment comment) {
+		
+	}
+	public ArrayList<Comment> getAllComments() {
+		return new ArrayList<Comment>();
+	}
+	public ArrayList<Comment> getCommentsOnReview(Review review) {
+		return new ArrayList<Comment>();
+	}
+}
