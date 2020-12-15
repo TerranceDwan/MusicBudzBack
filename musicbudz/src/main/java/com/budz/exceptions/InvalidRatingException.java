27e7af9e3cdc4eac19fcdba22df4c576d4230702
Jonah Neugeboren <jonahpie@gmail.com>
2020-12-15 16:29:18 -0500

    fixed custom exception

--- musicbudz/src/main/java/com/budz/exceptions/InvalidRatingException.java
+++ musicbudz/src/main/java/com/budz/exceptions/InvalidRatingException.java
@@ -2,9 +2,9 @@ package com.budz.exceptions;
 
 public class InvalidRatingException extends Exception{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	public InvalidRatingException() {
+		super("InvalidRatingException");
+	}
 }
--- musicbudz/src/test/java/com/budz/ReviewTests.java
+++ musicbudz/src/test/java/com/budz/ReviewTests.java
@@ -4,11 +4,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.budz.exceptions.InvalidRatingException;
+import com.budz.models.AlbumReview;
+import com.budz.models.SongReview;
+
 class ReviewTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void tooHighAlbumReviewThrowsError() {
+		Exception exception = assertThrows(InvalidRatingException.class, () -> {
+	        new AlbumReview(0, 0, "test", "test", 11, "test", "test");
+	    });
+
+	    String expectedMessage = "InvalidRatingException";
+	    String actualMessage = exception.getMessage();
+
+	    assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
+	@Test
+	void tooLowAlbumReviewThrowsError() {
+		Exception exception = assertThrows(InvalidRatingException.class, () -> {
+	        new AlbumReview(0, 0, "test", "test", -1, "test", "test");
+	    });
+
+	    String expectedMessage = "InvalidRatingException";
+	    String actualMessage = exception.getMessage();
+
+	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 
+	@Test
+	void tooHighSongReviewThrowsError() {
+		Exception exception = assertThrows(InvalidRatingException.class, () -> {
+	        new SongReview(0, 0, "test", "test", 11, "test", "test");
+	    });
+
+	    String expectedMessage = "InvalidRatingException";
+	    String actualMessage = exception.getMessage();
+
+	    assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
+	@Test
+	void tooLowSongReviewThrowsError() {
+		Exception exception = assertThrows(InvalidRatingException.class, () -> {
+	        new SongReview(0, 0, "test", "test", -1, "test", "test");
+	    });
+
+	    String expectedMessage = "InvalidRatingException";
+	    String actualMessage = exception.getMessage();
+
+	    assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
 }
