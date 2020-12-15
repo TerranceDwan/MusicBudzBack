b8bb924e83afca1bfbcef35feb6d554a0a5df292
TerranceDwan <terrance4869@gmail.com>
2020-12-15 11:23:36 -0500

    Init commit added Spring boot

---
+++ musicbudz/src/main/java/com/budz/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.budz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		
+		System.out.println("Music Budz Test 1");
+	}
+
+}
