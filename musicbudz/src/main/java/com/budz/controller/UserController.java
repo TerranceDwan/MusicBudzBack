a13fb601d91489cf45f15873dcc1a68ce4e69f86
mimel <imel.matt.r@gmail.com>
2020-12-15 15:48:28 -0600

    Added sample controller

---
+++ musicbudz/src/main/java/com/budz/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.budz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	@GetMapping("/test")
+	public String sampleCall() {
+		return "Successful call!";
+	}
+}
