8b84b4146c130e70adb53d4fafdf8817133829d9
mimel <imel.matt.r@gmail.com>
2020-12-22 09:44:29 -0600

    Added selenium tests.

---
+++ musicbudz/src/test/java/com/budz/selenium/LoginUITests.java
@@ -0,0 +1,46 @@
+package com.budz.selenium;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+class LoginUITests {
+	public static WebDriver driver;
+	
+	@BeforeAll
+	public static void init() {
+		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+		
+		driver.get("http://musicbudz.s3-website-us-east-1.amazonaws.com/login");
+	}
+	
+	@Test
+	void test() {
+		WebElement username = driver.findElement(By.id("login_username"));
+		WebElement password = driver.findElement(By.id("login_password"));
+		WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+		username.sendKeys("testUsername");
+		password.sendKeys("testPass");
+		submit.click();
+		
+		WebDriverWait w = new WebDriverWait(driver, 10);
+		WebElement modal = w.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#modal_container > .modal_wrapper")));
+		
+		Assertions.assertTrue(modal.isDisplayed());
+	}
+	
+	@AfterAll
+	public static void close() {
+		driver.quit();
+	}
+}
